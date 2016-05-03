ada150837a34ba7e79744f042473c4d7f51262a9
Joachim Bruno Strudsholm <joac0249@edu.easj.dk>
2016-05-03 13:11:38 +0200

    Addet Controllers

---
+++ Webservice/Controllers/MetalsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Webservice;
+
+namespace Webservice.Controllers
+{
+    public class MetalsController : ApiController
+    {
+        private StatueDBContext db = new StatueDBContext();
+
+        // GET: api/Metals
+        public IQueryable<Metal> GetMetal()
+        {
+            return db.Metal;
+        }
+
+        // GET: api/Metals/5
+        [ResponseType(typeof(Metal))]
+        public IHttpActionResult GetMetal(int id)
+        {
+            Metal metal = db.Metal.Find(id);
+            if (metal == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(metal);
+        }
+
+        // PUT: api/Metals/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutMetal(int id, Metal metal)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != metal.Metal_ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(metal).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MetalExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Metals
+        [ResponseType(typeof(Metal))]
+        public IHttpActionResult PostMetal(Metal metal)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Metal.Add(metal);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (MetalExists(metal.Metal_ID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = metal.Metal_ID }, metal);
+        }
+
+        // DELETE: api/Metals/5
+        [ResponseType(typeof(Metal))]
+        public IHttpActionResult DeleteMetal(int id)
+        {
+            Metal metal = db.Metal.Find(id);
+            if (metal == null)
+            {
+                return NotFound();
+            }
+
+            db.Metal.Remove(metal);
+            db.SaveChanges();
+
+            return Ok(metal);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool MetalExists(int id)
+        {
+            return db.Metal.Count(e => e.Metal_ID == id) > 0;
+        }
+    }
+}
---
+++ Webservice/Controllers/StatuesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Webservice;
+
+namespace Webservice.Controllers
+{
+    public class StatuesController : ApiController
+    {
+        private StatueDBContext db = new StatueDBContext();
+
+        // GET: api/Statues
+        public IQueryable<Statue> GetStatue()
+        {
+            return db.Statue;
+        }
+
+        // GET: api/Statues/5
+        [ResponseType(typeof(Statue))]
+        public IHttpActionResult GetStatue(int id)
+        {
+            Statue statue = db.Statue.Find(id);
+            if (statue == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(statue);
+        }
+
+        // PUT: api/Statues/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutStatue(int id, Statue statue)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != statue.Statue_ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(statue).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StatueExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Statues
+        [ResponseType(typeof(Statue))]
+        public IHttpActionResult PostStatue(Statue statue)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Statue.Add(statue);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (StatueExists(statue.Statue_ID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = statue.Statue_ID }, statue);
+        }
+
+        // DELETE: api/Statues/5
+        [ResponseType(typeof(Statue))]
+        public IHttpActionResult DeleteStatue(int id)
+        {
+            Statue statue = db.Statue.Find(id);
+            if (statue == null)
+            {
+                return NotFound();
+            }
+
+            db.Statue.Remove(statue);
+            db.SaveChanges();
+
+            return Ok(statue);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool StatueExists(int id)
+        {
+            return db.Statue.Count(e => e.Statue_ID == id) > 0;
+        }
+    }
+}
